1b12c7879aee846b69c162a6d607663688fa09e2
empijei <empijei@gmail.com>
2019-02-07 23:05:55 +0100

    Fixed dixem

--- tests/fixme/fixme.go
+++ tests/fixme/fixme.go
@@ -1,5 +1,31 @@
 package fixme
 
+import (
+	"sync"
+)
+
+// Excercise (medium): write and run a test that finds the 2 race conditions and the deadlock in the following function, and fix them.
+
 func SumOfAll(n int) (sum int) {
-	// TODO implement this
+	var (
+		mu  sync.Mutex
+		wg  sync.WaitGroup
+		tot int
+	)
+	wg.Add(n)
+
+	for i := 0; i < n; i++ {
+		if i%2 == 0 {
+			go func() {
+				mu.Lock()
+				defer mu.Unlock()
+				defer wg.Done()
+				tot += i
+			}()
+			continue
+		}
+		tot += i
+	}
+	wg.Wait()
+	return tot
 }
